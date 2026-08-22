dee4c32a3c9d871173cd914c9d652d6087c66932
Liyanaftva <118064000+Liyanaftva@users.noreply.github.com>
2026-08-22 18:16:23 +0530

    Complete JS & HTML, for testing

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-rock(){
-    console.log("Rock")
+const result_text = document.querySelector(".game-result-text")
+const score_player = document.querySelector(".game-score-player")
+const score_computer = document.querySelector(".game-score-computer")
+const computer_choice = document.querySelector(".game-choice-computer")
+
+function rock(){
+    const random = Math.floor(Math.random() * 3)    // number of choices = number you multiply by.
+    const player_choice = "rock"
+    if (random == 0 && player_choice == "rock"){
+        computer_choice.textContent = "Rock"
+        result_text.textContent = "Draw"
+    }
+    else if(random == 1 && player_choice == "rock"){
+        computer_choice.textContent = "Paper"
+        result_text.textContent = "You Lose"
+        score_computer.textContent = Number(score_computer.textContent) + 1
+    }
+    else{
+        computer_choice.textContent = "Scissors"
+        result_text.textContent = "You win"
+        score_player.textContent = Number(score_player.textContent) + 1
+    }
 }
-paper(){
-    cnsole.log("Paper")
+
+function paper(){
+    const random = Math.floor(Math.random() * 3)
+    const player_choice = "paper"
+    if (random == 1 && player_choice == "paper"){
+        computer_choice.textContent = "Paper"
+        result_text.textContent = "Draw"
+    }
+    else if(random == 2 && player_choice == "paper"){
+        computer_choice.textContent = "Scissors"
+        result_text.textContent = "You Lose"
+        score_computer.textContent = Number(score_computer.textContent) + 1
+    }
+    else{
+        computer_choice.textContent = "Rock"
+        result_text.textContent = "You win"
+        score_player.textContent = Number(score_player.textContent) + 1
+    }
 }
-scissors(){
-    console.log("Scissors")
+
+function scissors(){
+    const random = Math.floor(Math.random() * 3)
+    const player_choice = "scissors"
+    if (random == 2 && player_choice == "scissors"){
+        computer_choice.textContent = "Scissors"
+        result_text.textContent = "Draw"
+    }
+    else if(random == 0 && player_choice == "scissors"){
+        computer_choice.textContent = "Rock"
+        result_text.textContent = "You Lose"
+        score_computer.textContent = Number(score_computer.textContent) + 1
+    }
+    else{
+        computer_choice.textContent = "Paper"
+        result_text.textContent = "You win"
+        score_player.textContent = Number(score_player.textContent) + 1
+    }
 }
