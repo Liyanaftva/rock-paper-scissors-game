da125be2500ff37382eb9f2840c9866e5aa638ea
Liyanaftva <118064000+Liyanaftva@users.noreply.github.com>
2026-08-22 19:07:56 +0530

    test 8

--- script.js
+++ script.js
@@ -2,60 +2,103 @@ const result_text = document.querySelector(".game-result-text")
 const score_player = document.querySelector(".game-score-player")
 const score_computer = document.querySelector(".game-score-computer")
 const computer_choice = document.querySelector(".game-choice-computer")
+const player_hand = document.querySelector(".game-player-hand")
+const computer_hand = document.querySelector(".game-computer-hand")
+const countdown = document.querySelector(".game-countdown")
 
 function rock(){
     const random = Math.floor(Math.random() * 3)    // number of choices = number you multiply by.
     const player_choice = "rock"
-    if (random == 0 && player_choice == "rock"){
-        computer_choice.textContent = "Rock"
-        result_text.textContent = "Draw"
-    }
-    else if(random == 1 && player_choice == "rock"){
-        computer_choice.textContent = "Paper"
-        result_text.textContent = "You Lose"
-        score_computer.textContent = Number(score_computer.textContent) + 1
-    }
-    else{
-        computer_choice.textContent = "Scissors"
-        result_text.textContent = "You win"
-        score_player.textContent = Number(score_player.textContent) + 1
-    }
+    player_hand.textContent = "✊"
+    set_countdown()
+    setTimeout(
+        function(){
+            if (random == 0 && player_choice == "rock"){
+                computer_hand.textContent = "✊"
+                computer_choice.textContent = "Rock"
+                result_text.textContent = "Draw"
+            }
+            else if(random == 1 && player_choice == "rock"){
+                computer_hand.textContent = "✋"
+                computer_choice.textContent = "Paper"
+                result_text.textContent = "You Lose"
+                score_computer.textContent = Number(score_computer.textContent) + 1
+            }
+            else{
+                computer_hand.textContent = "✌️"
+                computer_choice.textContent = "Scissors"
+                result_text.textContent = "You win"
+                score_player.textContent = Number(score_player.textContent) + 1
+            }
+        }, 2000
+    )
 }
 
 function paper(){
     const random = Math.floor(Math.random() * 3)
     const player_choice = "paper"
-    if (random == 1 && player_choice == "paper"){
-        computer_choice.textContent = "Paper"
-        result_text.textContent = "Draw"
-    }
-    else if(random == 2 && player_choice == "paper"){
-        computer_choice.textContent = "Scissors"
-        result_text.textContent = "You Lose"
-        score_computer.textContent = Number(score_computer.textContent) + 1
-    }
-    else{
-        computer_choice.textContent = "Rock"
-        result_text.textContent = "You win"
-        score_player.textContent = Number(score_player.textContent) + 1
-    }
+    player_hand.textContent = "✋"
+    set_countdown()
+    setTimeout(
+        function(){
+            if (random == 1 && player_choice == "paper"){
+                computer_hand.textContent = "✋"
+                computer_choice.textContent = "Paper"
+                result_text.textContent = "Draw"
+            }
+            else if(random == 2 && player_choice == "paper"){
+                computer_hand.textContent = "✌️"
+                computer_choice.textContent = "Scissors"
+                result_text.textContent = "You Lose"
+                score_computer.textContent = Number(score_computer.textContent) + 1
+            }
+            else{
+                computer_hand.textContent = "✊"
+                computer_choice.textContent = "Rock"
+                result_text.textContent = "You win"
+                score_player.textContent = Number(score_player.textContent) + 1
+            }
+        },2000
+    )
 }
 
 function scissors(){
     const random = Math.floor(Math.random() * 3)
     const player_choice = "scissors"
-    if (random == 2 && player_choice == "scissors"){
-        computer_choice.textContent = "Scissors"
-        result_text.textContent = "Draw"
-    }
-    else if(random == 0 && player_choice == "scissors"){
-        computer_choice.textContent = "Rock"
-        result_text.textContent = "You Lose"
-        score_computer.textContent = Number(score_computer.textContent) + 1
-    }
-    else{
-        computer_choice.textContent = "Paper"
-        result_text.textContent = "You win"
-        score_player.textContent = Number(score_player.textContent) + 1
-    }
+    player_hand.textContent = "✌️"
+    set_countdown()
+    setTimeout(function(){
+            if (random == 2 && player_choice == "scissors"){
+                computer_hand.textContent = "✌️"
+                computer_choice.textContent = "Scissors"
+                result_text.textContent = "Draw"
+            }
+            else if(random == 0 && player_choice == "scissors"){
+                computer_hand.textContent = "✊"
+                computer_choice.textContent = "Rock"
+                result_text.textContent = "You Lose"
+                score_computer.textContent = Number(score_computer.textContent) + 1
+            }
+            else{
+                computer_hand.textContent = "✋"
+                computer_choice.textContent = "Paper"
+                result_text.textContent = "You win"
+                score_player.textContent = Number(score_player.textContent) + 1
+            }
+        }, 2000
+    )
+}
+
+function set_countdown(){
+    countdown.textContent = 3
+    setTimeout(
+        function(){
+            countdown.textContent -= 1
+        }, 1000
+    )
+    setTimeout(
+        function(){
+            countdown.textContent -= 1
+        }, 2000
+    )
 }
